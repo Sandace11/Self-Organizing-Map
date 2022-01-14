7e3333c4a016bc5a53cadd4e3339ee1bfaffdb81
“[Sandesh <075bct075.sandesh@pcampus.edu.np>
2022-01-14 21:23:35 +0545

    Added comments to node.js. github repo initialized. 1st push to github

--- node.js
+++ node.js
@@ -6,16 +6,16 @@ class Node {
         this.m_iRight = rgt;
         this.m_iBottom = bot;
 
-        this.m_dWeights = []; //this nodes weights
-        this.m_dx = this.m_iLeft + (this.m_iRight - this.m_iLeft) / 2; //position in lattice
-        this.m_dy = this.m_iTop + (this.m_iBottom - this.m_iTop) / 2; //position in lattice
+        this.m_dWeights = []; //this nodes weights (3 valued vector in my case(defined in constants.js) but kept flexible to accomdate higher dimensional inputs.
+        this.m_dx = this.m_iLeft + (this.m_iRight - this.m_iLeft) / 2; //X position of node in lattice
+        this.m_dy = this.m_iTop + (this.m_iBottom - this.m_iTop) / 2; //Y position of node in lattice
 
         for (let w = 0; w < NumWeights; w++) { // initialize weights to random value
             this.m_dWeights.push(Math.random().toFixed(2))
         }
     }
 
-    render() {  //Render the node on the screen
+    render() {  //Render the node on the screen. p5 ko rect function use gareko. takes (topLeftKoX, topleftKoY, width, height)
         red = this.m_dWeights[0] * 255;
         green = this.m_dWeights[1] * 255;
         blue = this.m_dWeights[2] * 255;
@@ -23,14 +23,19 @@ class Node {
         rect(this.m_iLeft, this.m_iTop, constCellWidth, constCellHeight);
     }
 
-    calculateDistance(inputVector) {
-        let distance = 0;
-    
+    calculateDistance(inputVector) {  //euclidean dist betn current input vector and this 
+        let distance = 0;           // node's weight. note : yaa square root haalera return garne haina?
+                                        // Every iteration ko best fit node nikaalna kaam lagxa 
         for (let i = 0; i < this.m_dWeights.length; i++) {
-            distance += (InputVector[i] - m_dWeights[i]) *
-                        (InputVector[i] - m_dWeights[i]);
+            distance += (inputVector[i] - this.m_dWeights[i]) *
+                (inputVector[i] - this.m_dWeights[i]);
         }
-    
         return distance;
+    }
+
+    AdjustWeights(target, learningRate, influence) {      //for adjustment of weights during learning. target is the aaileko input vector. 
+        for (let w = 0; w < target.length; w++) {           //learningRate is the current learningRate as it isn't constant(only at beginning it was constStartLearningRate ) and its value decays with iterations
+            this.m_dWeights[w] += learningRate * influence * (target[w] - this.m_dWeights[w]); //influence is the topological neighbourhood. also not constant through iterations
+        }                                                                       //https://youtu.be/g8O6e9C_CfY?t=584 for reference of this weight update function.
     }
 }
