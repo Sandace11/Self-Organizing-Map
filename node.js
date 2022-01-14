b2c2130899f4c99f72ce90d9abb3a5904c1f40ee
“[Sandesh <075bct075.sandesh@pcampus.edu.np>
2022-01-14 20:18:33 +0545

    intial commit

---
+++ node.js
@@ -0,0 +1,12 @@
+
+class Node {
+    constructor(lft, rgt, top, bot, NumWeights) {
+        this.m_iLeft = lft;
+        this.m_iTop = top;
+        this.m_iRight = rgt;
+        this.m_iBottom = bot;
+           
+    }
+
+
+}
